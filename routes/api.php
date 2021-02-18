3e98c8ff3f48eb6aa56ce91395f494d8ad863720
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2021-02-18 12:19:02 +0200

    Clean unuserd

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,6 @@ Route::post('forgotPassword', '\App\Http\Controllers\Auth\ForgotPasswordControll
 
 // Pages
 Route::get('home', 'HomeController@home');
-// Route::get('reviews', 'HomeController@reviews');
-// Route::get('faqs', 'HomeController@faqs');
 Route::get('informations', 'HomeController@informations');
 Route::get('pages/{id}', 'HomeController@pages');
 Route::get('metas', 'HomeController@metas');
@@ -33,7 +31,6 @@ Route::post('send-contact', 'HomeController@sendContactMessage');
 Route::post('newsletter', 'HomeController@newsletter');
 
 Route::get('landing-page', 'HomeController@landingPage');
-// Route::get('landing-page-search', 'HomeController@landingPageSearch');
 Route::get('services', 'HomeController@services');
 Route::get('freelancers', 'HomeController@freelancers');
 Route::get('skills', 'HomeController@skills');
@@ -105,36 +102,4 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('freelancer/{id}', 'HomeController@freelancer');
     Route::get('freelancer-jobs/{id}', 'HomeController@freelancerJobs');
     Route::get('client-jobs/{id}', 'HomeController@clientJobs');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Route::post('create-product', 'HomeController@createProduct');
-    // Route::post('add-bid/{id}', 'HomeController@addBid');
-    // Route::post('add-review/{id}', 'HomeController@addReview');
-    // Route::get('current-user-bids', 'HomeController@currentUserBids');
-    // Route::get('pending-user-bids', 'HomeController@pendingUserBids');
-    // Route::get('finished-user-bids', 'HomeController@finishedUserBids');
-    // Route::get('upcoming-my-bids', 'HomeController@upcomingMyBids');
-    // Route::get('current-my-bids', 'HomeController@currentMyBids');
-    // Route::get('past-my-bids', 'HomeController@pastMyBids');
-    // Route::get('winning-bids', 'HomeController@winningBids');
-    // Route::post('add-or-remove-favourites/{id}', 'HomeController@addOrRemoveFavourites');
-    // Route::get('my-favourites', 'HomeController@myFavourites');
-    // Route::get('dashboard', 'HomeController@dashboard');
-    // Route::post('update-personal-information', 'HomeController@updatePersonalInformation');
-    // Route::post('update-password', 'HomeController@updatePassword');
-    // Route::post('charge-balance', 'HomeController@chargeBalance');
-    // Route::get('transactions', 'HomeController@transactions');
-    // Route::post('subscription', 'HomeController@subscription');
 });
